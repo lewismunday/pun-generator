5cbc2ecb13f186ebe131dd08bab600451396c687
Lewis Munday <lewisjohnmunday@gmail.com>
2022-01-30 18:35:29 +0000

    Started working on JavaScript for pun generator

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello world!')
+
+//Sets variables for called upon intiators
+
+const randomNumber = Math.floor(Math.random() * 101)
+const punTypes = ['Bad Puns', 'Dog Puns', 'Food Puns']
+
+//Creating object for puns
+
+const badPuns = {
+    1: ['Question?', 'Response'],
+    2: ['Question2?', 'Response2']
+}
+
+const dogPuns = {
+    1: ['Question?', 'Response'],
+    2: ['Question2?', 'Response2']
+}
+
+const foodPuns = {
+    1: ['Question?', 'Response'],
+    2: ['Question2?', 'Response2']
+}
+
+//console.log(randomNumber)
