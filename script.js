4da941daf3ec94146632ddfff5b5cbb4faf679df
Lewis Munday <lewisjohnmunday@gmail.com>
2022-01-30 20:22:56 +0000

    Added basic function to log array

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 
 //Sets variables for called upon intiators
 
-const randomNumber = Math.floor(Math.random() * 101)
+const randomNumber = Math.floor(Math.random() * 2)
 const punTypes = ['Bad Puns', 'Dog Puns', 'Food Puns']
 
 //Creating object for puns
 
 const badPuns = {
-    1: ['Question?', 'Response'],
-    2: ['Question2?', 'Response2']
+    0: ['Question?', 'Response'],
+    1: ['Question2?', 'Response2']
 }
 
 const dogPuns = {
-    1: ['Question?', 'Response'],
-    2: ['Question2?', 'Response2']
+    0: ['Question?', 'Response'],
+    1: ['Question2?', 'Response2']
 }
 
 const foodPuns = {
-    1: ['Question?', 'Response'],
-    2: ['Question2?', 'Response2']
+    0: ['Question?', 'Response'],
+    1: ['Question2?', 'Response2']
 }
 
 //console.log(randomNumber)
 
 const generator = input => {
-    console.log('Yipee!')
+    const findIndex = punTypes.indexOf(input)
+    if (input == punTypes[findIndex]){
+        console.log(badPuns[randomNumber])
+    } else {
+        console.log('WRONG')
+    }
 }
 
-generator('blah')
+generator('Bad Puns')
